ceae8e08a773f35ea2cf8f12f900d5c52d3a8e27
Misael-GC <misael.gomez1623@gmail.com>
2024-01-11 13:16:10 -0600

    creamos una variable tipo persona que no es puntero

---
+++ POO/estructura.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Persona{
+    string nombre;
+    int edad;
+};
+
+int main()
+{
+    //Persona *p = new Persona(); //Reserva memoria
+    Persona p = Persona(); //CREAMOS UNA VARIABLE TIPO PERSONA QUE NO ES PUNTERO
+    p.nombre = "Greñas"; //Asignar a ese valor
+    p.edad = 24; //Asignar a ese valor
+    cout << p.nombre <<endl;
+    cout<<"La persona tiene: "<<p.edad<<endl;
+
+}
