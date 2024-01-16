0451a3bf2a52b2f982bf39def82a70af0643ab27
Misael-GC <misael.gomez1623@gmail.com>
2024-01-16 16:48:33 -0600

    datos y print en c++

---
+++ PracticoC++/librerias.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"           _,    _   _    ,_"<< endl;
+    cout <<"  .o888P     Y8o8Y     Y888o."<< endl;
+    cout <<" d88888      88888      88888b"<< endl;
+    cout <<"d888888b_  _d88888b_  _d888888b"<< endl;
+    cout <<"8888888888888888888888888888888"<< endl;
+    cout <<"8888888888888888888888888888888"<< endl;
+    cout <<"YJGS8PY888P-Y888P-Y888P-Y8888P"<< endl;
+    cout <<" Y888   '8'   Y8P   '8'   888Y"<< endl;
+    cout <<"  '8o          V          o8'"<< endl;
+    cout <<"     `                     `"<< endl;
+
+
+
+    return 0;
+}
+
+//https://www.asciiart.eu/mythology/dragons
