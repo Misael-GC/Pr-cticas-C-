9f4d5d73ebd944895411b1f0db231c10b515bdf6
Misael-GC <misael.gomez1623@gmail.com>
2024-01-16 21:27:16 -0600

    recibir vslores del usuario parte 1

---
+++ PracticoC++/introduce_dato.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Edad =0;
+    cout << "Introduce tu edad:" << endl;
+
+    cin >> Edad;
+
+    cout <<"Que joven, apenas tienes " << Edad << endl;
+}
