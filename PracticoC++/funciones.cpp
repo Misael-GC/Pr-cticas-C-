8bac7025b30b1a5a7fec5794cae453a618cd1e6b
Misael-GC <misael.gomez1623@gmail.com>
2024-01-18 17:41:50 -0600

    funciones de tipo booleano

--- PracticoC++/funciones.cpp
+++ PracticoC++/funciones.cpp
@@ -4,12 +4,31 @@
 using namespace std;
 
 //void no regresa nada, solo ejecuta los comandos denrtro
-void PrintCuteMessage(){
+/*void PrintCuteMessage(){
     cout << "Eres increible!"<<endl;
     cout << ":D"<<endl;
+}
+*/
+
+bool IsPlayerDead(int hp){
+    if (hp <= 0) {
+        return true;
+    }else{
+        return false;
+    }
 }
 
 int main()
 {
-    PrintCuteMessage();
+   // PrintCuteMessage();
+    int TotalLife = 3;
+
+   if (TotalLife <= 0)
+   {
+    cout <<"Game Over";
+   } else{
+    cout <<"Sigue jugando crack";
+   }
+   
+   return 0;
 }
