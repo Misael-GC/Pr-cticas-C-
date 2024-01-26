82c5146dadf7b9c0f7dddcbb47da8c0b1972f330
Misael-GC <misael.gomez1623@gmail.com>
2024-01-25 22:07:16 -0600

    fibonacci

--- PracticoC++/fibonacci.cpp
+++ PracticoC++/fibonacci.cpp
@@ -3,9 +3,22 @@
 
 using namespace std;
 
+int fibonacci(int n){
+
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else{
+       int result =  fibonacci(n-1) + fibonacci(n-2);
+        return result;
+    }
+}
+
 int main()
 {
-
-
+    int result = fibonacci(7);
+    cout << result << endl;
     return 0;
 }
+
--- PracticoC++/funciones.cpp
+++ PracticoC++/funciones.cpp
@@ -10,25 +10,33 @@ using namespace std;
 }
 */
 
-bool IsPlayerDead(int hp){
+/*bool IsPlayerDead(int hp){
     if (hp <= 0) {
         return true;
     }else{
         return false;
     }
+}*/
+
+void Print(string message){
+    cout << message << endl;
 }
 
 int main()
 {
    // PrintCuteMessage();
-    int TotalLife = 3;
+
+
+/*   int TotalLife = 3;
 
    if (TotalLife <= 0)
    {
     cout <<"Game Over";
    } else{
     cout <<"Sigue jugando crack";
-   }
-   
+   }*/
+
+
+   Print("Hola :)");
    return 0;
 }
